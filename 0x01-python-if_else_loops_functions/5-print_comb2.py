ba3f7e8d1f2bcea9b2902e4eec3f2e74b4756bb8
Nightburnn <nightburn2003@gmail.com>
2022-11-29 08:40:07 -0800

    7

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # printing 0 to 99
-for num in range(0, 100):
-    if num == 99:
-        print("{}".format(num))
-        continue
-        print("{:02}, ".format(num), end="")
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+else:
+    print("{:02}".format(number), end=", ")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    number = ord(c)
+    if number in range(97, 123):
+        return True
+    return False
