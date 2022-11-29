2ce55884153c21902b6342062e9afadb2bedc225
Nightburnn <nightburn2003@gmail.com>
2022-11-29 12:38:08 -0800

    fixing5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# printing 0 to 99
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for i in range(100):
+    if i == 99:
+        print(i)
 else:
-    print("{:02}".format(number), end=", ")
+    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
