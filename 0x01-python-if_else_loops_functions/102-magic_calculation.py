6d0abb5e0fc65e027dfd0bc7314dbc5235d68ac6
Nightburnn <nightburn2003@gmail.com>
2022-11-29 12:24:54 -0800

    dargon ball is mid

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+
+    """Match bytecode provided by Holberton School."""
+
+    if a < b:
+
+        return (c)
+
+    if c > b:
+
+        return (a + b)
+
+    return (a*b - c)
