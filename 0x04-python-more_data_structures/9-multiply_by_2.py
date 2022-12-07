874937ef0115fc2e7fd38307e68f3ce0227cfb6d
Nightburnn <nightburn2003@gmail.com>
2022-12-07 04:02:51 -0800

    fixing error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-    for i in list_keys:
-        new_dir[i] *= 2
-        return (new_dir)
+    return {x: y*2 for x, y in a_dictionary.items()}
