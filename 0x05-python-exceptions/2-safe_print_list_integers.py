a32ae19f1974ff0395bc5151968411a6098609b6
Nightburnn <nightburn2003@gmail.com>
2022-12-19 08:56:52 -0800

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    test = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            test += 1
+        except (ValueError, TypeError):
+            continue
+        print("")
+        return (test)
