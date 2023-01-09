ccfb6620a7124f305c7a23abaf58ed618af49b31
Nightburnn <nightburn2003@gmail.com>
2023-01-09 09:58:39 -0800

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
-
-def print_sorted(self):
-
-    """Print a list in sorted ascending order."""
-
-
-print(sorted(self))
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
