18e61b17cfd0b921fa0c4820c33bcff4c3e4990e
Nightburnn <nightburn2003@gmail.com>
2023-01-10 08:40:15 -0800

    Add function to read JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a JSON file-reading function."""
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file"""
+    with open(filename) as f:
+        return json.load(f)
