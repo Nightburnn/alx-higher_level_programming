76f2004038b47732547beccc087f435d9825346b
Nightburnn <nightburn2003@gmail.com>
2023-01-10 10:27:51 -0800

    error

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+"""Search and update"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
-    Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
-    """
+    """inserts a line of text to a file, after each line with a spc. string"""
     text = ""
-    with open(filename) as r:
-        for line in r:
+    with open(filename, "r") as file:
+        for line in file:
             text += line
             if search_string in line:
                 text += new_string
-                with open(filename, "w") as w:
-                    w.write(text)
+    with open(filename, "w") as file:
+        file.write(text)
