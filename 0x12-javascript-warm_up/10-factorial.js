cb790cc5fb37999d108cce4880ddda4c3edc2851
Nightburnn <nightburn2003@gmail.com>
2023-03-13 08:49:37 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
