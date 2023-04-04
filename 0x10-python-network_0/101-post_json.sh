2b19b4f846401d6df7b4eb60d420cb3df55d3a88
Nightburnn <Nightburn2003@gmail.com>
2023-04-04 09:55:00 +0000

    display the final result

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "%(cat "$2")" "$1"
+# JSON POST request to a URL passed as the first arg and displays the body of the response
+curl -s -H "Content-Type: application/json" -d @"$2" "$1".
